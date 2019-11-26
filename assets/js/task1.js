dad964d4a02d1361b40d3aa72c5c4957260851e0
crysis <crysiswarhead020304050@gmail.com>
2019-11-26 17:02:11 +0200

    First class commit

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -1,91 +1,152 @@
 'use strict';
 
-/*1*/
-function factorial(n) {
-    if (n === 0) {
-        return 1;
+const SOCIAL_NETWORKS = Object.freeze( {
+    FACEBOOK: "FACEBOOK",
+    TWITTER: "TWITTER",
+    SKYPE: "SKYPE",
+    GOOGLE_PLUS: "GOOGLE_PLUS",
+});
+
+
+const users = [
+    {
+        id: 1,
+        name: "Name1",
+        surname: "Surname1",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.FACEBOOK,
+            SOCIAL_NETWORKS.TWITTER,
+            SOCIAL_NETWORKS.SKYPE,
+            SOCIAL_NETWORKS.GOOGLE_PLUS,
+        ]
+    },
+    {
+        id: 2,
+        name: "Name2",
+        surname: "Surname2",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.FACEBOOK,
+            SOCIAL_NETWORKS.TWITTER,
+            SOCIAL_NETWORKS.SKYPE,
+        ]
+    },
+    {
+        id: 3,
+        name: "Name3",
+        surname: "Surname3",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.FACEBOOK,
+            SOCIAL_NETWORKS.TWITTER,
+
+        ]
+    },
+    {
+        id: 4,
+        name: "Name4",
+        surname: "Surname4",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.FACEBOOK,
+
+        ]
+    },
+    {
+        id: 5,
+        name: "Name5",
+        surname: "Surname5",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.FACEBOOK,
+            SOCIAL_NETWORKS.TWITTER,
+            SOCIAL_NETWORKS.SKYPE,
+            SOCIAL_NETWORKS.GOOGLE_PLUS,
+        ]
+    },
+    {
+        id: 6,
+        name: "Name6",
+        surname: "Surname6",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.SKYPE,
+            SOCIAL_NETWORKS.GOOGLE_PLUS,
+        ]
+    },
+    {
+        id: 7,
+        name: "Name7",
+        surname: "Surname7",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.GOOGLE_PLUS,
+        ]
+    },
+    {
+        id: 8,
+        name: "Name8",
+        surname: "Surname8",
+        avatar: "",
+        description: "",
+        socialNetworkList: [
+            SOCIAL_NETWORKS.FACEBOOK,
+            SOCIAL_NETWORKS.TWITTER,
+            SOCIAL_NETWORKS.SKYPE,
+            SOCIAL_NETWORKS.GOOGLE_PLUS,
+        ]
+    },
+];
+
+const userListElement = document.getElementsByClassName("cardContainer");
+
+users.forEach(
+    (user) => {
+        userListElement.append(createUserListElement(user));
     }
-    else {
-        return n*factorial(n-1);
-    }
+);
+
+function createUserListElement(user) {
+    const userImageContainerElement = document.createElement("div");
+    userImageContainerElement.classList.add("userCard");
 }
 
-/*2*/
-    function numbersInDirectOrder(x,y) {
-        if (isNaN(+x) || isNaN(+y)) {
-            return console.log("Not a number");
-        }
-        else {
-            if (y >= x) {
-                console.log(x);
-                x++;
-                numbersInDirectOrder(x,y);
-            }
-        }
-    }
+function createUserImageElement(user) {
+    const userImageContainerElement = document.createElement("div");
+    userImageContainerElement.classList.add("userAvatar");
+    const userImage = document.createElement("img");
+    userImage.setAttribute("src", "");
+    userImage.setAttribute("alt", "user avatar")
+    userImageContainerElement.append(userImage);
 
-    function numbersInReverseOrder(x,y) {
-        if (isNaN(+x) || isNaN(+y)) {
-            return console.log("Not a number");
-        }
-        else {
-            if (y >= x) {
-                console.log(y);
-                y--;
-                numbersInReverseOrder(x,y);
-            }
-        }
-    }
+    return userImageContainerElement;
+}
 
+function createUserFullName(user) {
+    const userFullNameContainerElement = document.createElement("div");
+    userFullNameContainerElement.classList.add("fullName");
+    const userFullName = document.createElement("h4");
+    userFullName.innerText = `${user.name} ${user.surname}`;
+    userFullNameContainerElement.append(userFullName);
 
-/*3*/
-    function reverseNumber(n) {
-        if (isNaN(+n)) {
-            return console.log("Not a number");
-        }
-        else if(n<10){
-            return n;
-        }
-        else {
-            const result = n%10;
-            const nextNumber = (n - result) / 10;
+    return userFullNameContainerElement;
 
-            const str = result + reverseNumber(nextNumber).toString();
+}
 
-            return Number(str);
-        }
+function createDescription(user) {
+    const userDescriptionContainer = document.createElement("div");
+    userDescriptionContainer.classList.add("description");
+    const userDescription = document.createElement("p");
+    userDescription.innerText = `${user.description}`;
+    userDescriptionContainer.append(userDescription);
 
-    }
-
-/*4*/
-
-
-    function sum(n) {
-        if (isNaN(+n)) {
-            return console.log("Not a number");
-        }
-        else if (n < 10) {
-            return n;
-        }
-        else {
-            const result = n % 10;
-
-            return result + sum((n - result) / 10);
-        }
-
-    }
-
-/*5*/
-
-    function brackets(n) {
-
-        if(n=== 1) {
-            return "()";
-
-        }
-
-        return `(${brackets(n - 1)})`;
-
-    }
-
-    
+    return userDescriptionContainer;
+}
