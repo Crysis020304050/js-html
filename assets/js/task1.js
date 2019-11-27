922ee5b92e82fd83d7cd3880941fb8c4532845fb
Crysis020304050 <crysiswarhead020304050@gmail.com>
2019-11-27 12:45:18 +0200

    Commit before editing

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -13,100 +13,100 @@ const users = [
         id: 1,
         name: "Name1",
         surname: "Surname1",
-        avatar: "",
-        description: "",
+        avatar: "https://www.e-xecutive.ru/uploads/article/image/1987310/thumb_Bocharsky.jpg",
+        description: "dmfbgvjdfoijgo sdfiogjdfsjgiodf siodfgjfdjg djgfidjf fjdfk lasm",
         socialNetworkList: [
-            SOCIAL_NETWORKS.FACEBOOK,
-            SOCIAL_NETWORKS.TWITTER,
-            SOCIAL_NETWORKS.SKYPE,
-            SOCIAL_NETWORKS.GOOGLE_PLUS,
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
+            {type : SOCIAL_NETWORKS.TWITTER, href: "#"},
+            {type : SOCIAL_NETWORKS.SKYPE, href: "#"},
+            {type : SOCIAL_NETWORKS.GOOGLE_PLUS, href: "#"},
         ]
     },
     {
         id: 2,
         name: "Name2",
         surname: "Surname2",
-        avatar: "",
-        description: "",
+        avatar: "https://pristor.ru/wp-content/uploads/2019/09/%D0%9A%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%8B%D0%B5-%D0%BC%D1%83%D0%B6%D1%87%D0%B8%D0%BD%D1%8B-%D0%B7%D0%B0-40-%D1%84%D0%BE%D1%82%D0%BE-%D0%B4%D0%BB%D1%8F-%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B8015.jpg",
+        description: ";j[h; aaea plgjhp zbsgvg pgjh[aks apa[sk",
         socialNetworkList: [
-            SOCIAL_NETWORKS.FACEBOOK,
-            SOCIAL_NETWORKS.TWITTER,
-            SOCIAL_NETWORKS.SKYPE,
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
+            {type : SOCIAL_NETWORKS.TWITTER, href: "#"},
+            {type : SOCIAL_NETWORKS.SKYPE, href: "#"},
         ]
     },
     {
         id: 3,
         name: "Name3",
         surname: "Surname3",
-        avatar: "",
-        description: "",
+        avatar: "https://cache.kwork.ru/pics/t3/00/207103-3.jpg",
+        description: "dlsadlfp apsl[d; fd<ze [;g[ g<tgzy plplp lplf k<jnzkxnk p;h[j[ osidiui hpfgohpo isaudi ff",
         socialNetworkList: [
-            SOCIAL_NETWORKS.FACEBOOK,
-            SOCIAL_NETWORKS.TWITTER,
-
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
+            {type : SOCIAL_NETWORKS.TWITTER, href: "#"},
         ]
     },
     {
         id: 4,
         name: "Name4",
         surname: "Surname4",
-        avatar: "",
-        description: "",
+        avatar: "https://cdn.lifehacker.ru/wp-content/uploads/2018/04/woman-3083375_1920_1524655476-e1524655499103.jpg",
+        description: "o];l']ol ;'l]' fasrdrs cnvkb pyuopyupy uwyewye wrpwrp ip[y",
         socialNetworkList: [
-            SOCIAL_NETWORKS.FACEBOOK,
-
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
         ]
     },
     {
         id: 5,
         name: "Name5",
         surname: "Surname5",
-        avatar: "",
-        description: "",
+        avatar: "https://img2.goodfon.ru/wallpaper/nbig/5/66/emily-rudd-girl-devushka-vzglyad-437.jpg",
+        description: "klhjloghk rdwrs oyhpoup pljphlp jdjxg dktged irjieuir eioteitrie iwerierie eirier",
         socialNetworkList: [
-            SOCIAL_NETWORKS.FACEBOOK,
-            SOCIAL_NETWORKS.TWITTER,
-            SOCIAL_NETWORKS.SKYPE,
-            SOCIAL_NETWORKS.GOOGLE_PLUS,
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
+            {type : SOCIAL_NETWORKS.TWITTER, href: "#"},
+            {type : SOCIAL_NETWORKS.SKYPE, href: "#"},
+            {type : SOCIAL_NETWORKS.GOOGLE_PLUS, href: "#"},
         ]
     },
     {
         id: 6,
         name: "Name6",
         surname: "Surname6",
-        avatar: "",
-        description: "",
+        avatar: "https://dok7xy59qfw9h.cloudfront.net/566/203/371/-69996995-1t7nemh-jdhb82ra01be6bk/original/COhrPJYTohM.jpg",
+        description: ";k[lpk[ sgvdfstd [yup[ypu[y wyettwetw jcnvjcnvj hl;jphljp weyywyeyu rotorito wuey7wye7w eoitroeito wueyuwyeu fohkoghko lgjlkpgh",
         socialNetworkList: [
-            SOCIAL_NETWORKS.SKYPE,
-            SOCIAL_NETWORKS.GOOGLE_PLUS,
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
+            {type : SOCIAL_NETWORKS.TWITTER, href: "#"},
+            {type : SOCIAL_NETWORKS.SKYPE, href: "#"},
         ]
     },
     {
         id: 7,
         name: "Name7",
         surname: "Surname7",
-        avatar: "",
-        description: "",
+        avatar: "https://www.cosmo.com.ua/i/publications/22/830_550/opredeli-svoe-nastroenie-po-avatarke-10751.jpg",
+        description: "[ki[kp yegtwgfet pylupyp kdjfkldkf dlfklodfktgo oee",
         socialNetworkList: [
-            SOCIAL_NETWORKS.GOOGLE_PLUS,
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
         ]
     },
     {
         id: 8,
         name: "Name8",
         surname: "Surname8",
-        avatar: "",
-        description: "",
+        avatar: "https://trikky.ru/wp-content/blogs.dir/1/files/2017/12/1-680-3.jpg",
+        description: "[i;o[i shudhush euyrue ogkhot jdnfjdfj pghpg ue koghkotr ekrjie ortko",
         socialNetworkList: [
-            SOCIAL_NETWORKS.FACEBOOK,
-            SOCIAL_NETWORKS.TWITTER,
-            SOCIAL_NETWORKS.SKYPE,
-            SOCIAL_NETWORKS.GOOGLE_PLUS,
+            {type : SOCIAL_NETWORKS.FACEBOOK, href: "#"},
+            {type : SOCIAL_NETWORKS.TWITTER, href: "#"},
+            {type : SOCIAL_NETWORKS.SKYPE, href: "#"},
+            {type : SOCIAL_NETWORKS.GOOGLE_PLUS, href: "#"},
         ]
     },
 ];
 
-const userListElement = document.getElementsByClassName("cardContainer");
+const userListElement = document.getElementById("userCardContainer");
+
 
 users.forEach(
     (user) => {
@@ -114,16 +114,32 @@ users.forEach(
     }
 );
 
+
+
+
+
 function createUserListElement(user) {
-    const userImageContainerElement = document.createElement("div");
-    userImageContainerElement.classList.add("userCard");
+    const userContainerElement = document.createElement("div");
+    userContainerElement.classList.add("userCard");
+    const userImageElement = createUserImageElement(user);
+    const userFullName = createUserFullName(user);
+    const userDescription = createUserDescription(user);
+    const userSocialNetworks = createUserSocialNetworks(user);
+
+    userContainerElement.append(userImageElement);
+    userContainerElement.append(userFullName);
+    userContainerElement.append(userDescription);
+    userContainerElement.append(userSocialNetworks);
+
+    return userContainerElement;
+
 }
 
 function createUserImageElement(user) {
     const userImageContainerElement = document.createElement("div");
     userImageContainerElement.classList.add("userAvatar");
     const userImage = document.createElement("img");
-    userImage.setAttribute("src", "");
+    userImage.setAttribute("src", `${user.avatar}`);
     userImage.setAttribute("alt", "user avatar")
     userImageContainerElement.append(userImage);
 
@@ -141,7 +157,7 @@ function createUserFullName(user) {
 
 }
 
-function createDescription(user) {
+function createUserDescription(user) {
     const userDescriptionContainer = document.createElement("div");
     userDescriptionContainer.classList.add("description");
     const userDescription = document.createElement("p");
@@ -150,3 +166,50 @@ function createDescription(user) {
 
     return userDescriptionContainer;
 }
+
+function createUserSocialNetworks(user) {
+    const userSocialNetworkContainer = document.createElement("div");
+    userSocialNetworkContainer.classList.add("socialNetworksList");
+    user.socialNetworkList.forEach( socialLink => {
+        if(socialLink.type === SOCIAL_NETWORKS.FACEBOOK) {
+            const userFacebook = document.createElement("a");
+            userFacebook.setAttribute("href", `${socialLink.href}`);
+            const facebookImage = document.createElement("img");
+            facebookImage.setAttribute("src", "assets/images/icons/facebook.png");
+            facebookImage.setAttribute("alt", "facebook");
+            userFacebook.append(facebookImage);
+            userSocialNetworkContainer.append(userFacebook);
+        }
+        if(socialLink.type === SOCIAL_NETWORKS.TWITTER) {
+            const userTwitter = document.createElement("a");
+            userTwitter.setAttribute("href", `${socialLink.href}`);
+            const twitterImage = document.createElement("img");
+            twitterImage.setAttribute("src", "assets/images/icons/twitter.png");
+            twitterImage.setAttribute("alt", "twitter");
+            userTwitter.append(twitterImage);
+            userSocialNetworkContainer.append(userTwitter);
+        }
+        if(socialLink.type === SOCIAL_NETWORKS.SKYPE) {
+            const userSkype = document.createElement("a");
+            userSkype.setAttribute("href", `${socialLink.href}`);
+            const skypeImage = document.createElement("img");
+            skypeImage.setAttribute("src", "assets/images/icons/skype.png");
+            skypeImage.setAttribute("alt", "skype");
+            userSkype.append(skypeImage);
+            userSocialNetworkContainer.append(userSkype);
+        }
+        if(socialLink.type === SOCIAL_NETWORKS.GOOGLE_PLUS) {
+            const userGogglePlus = document.createElement("a");
+            userGogglePlus.setAttribute("href", `${socialLink.href}`);
+            const googlePlusImage = document.createElement("img");
+            googlePlusImage.setAttribute("src", "assets/images/icons/google-plus.png");
+            googlePlusImage.setAttribute("alt", "google-plus");
+            userGogglePlus.append(googlePlusImage);
+            userSocialNetworkContainer.append(userGogglePlus);
+        }
+    });
+
+    return userSocialNetworkContainer;
+}
+
+
